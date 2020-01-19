e1f8ac75d018f534a35d12be18461ac24935864e
kanjeranian <oilscius@gmail.com>
2020-01-20 06:50:05 +0700

    format code

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PrevScore from './PrevScore';
 import engineerLogo from './img/engineer.svg';
 import favoriteIcon from './img/favorite.svg';
